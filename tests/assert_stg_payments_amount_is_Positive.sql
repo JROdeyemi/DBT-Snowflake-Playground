2b3c7b8d89c91abcf4d2c4c0c92ad9494ee96514
Joshua Odeyemi <joshodeyemi20@gmail.com>
2025-04-17 09:39:16 +0000

    Added tests to sources and introduced singular tests to payments

---
+++ tests/assert_stg_payments_amount_is_Positive.sql
@@ -0,0 +1,10 @@
+WITH payments AS (
+    SELECT *
+    FROM {{ ref('stg_stripe__payments') }}
+)
+
+SELECT OrderID,
+        SUM(Amount) AS Total_Amount
+FROM payments 
+GROUP BY OrderID 
+HAVING Total_Amount < 0
